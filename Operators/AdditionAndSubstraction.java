18fb459cb6939364c68df10244913b50443516b6
riyazshaikh4523 <147690876+riyazshaikh4523@users.noreply.github.com>
2023-10-12 11:48:46 +0530

    Create AdditionAndSubstraction.java

---
+++ Operators/AdditionAndSubstraction.java
@@ -0,0 +1,10 @@
+class Addition{
+public static void main(String args[]){
+int a=15;
+int b=30;
+int addition=a+b;
+int substraction=a-b;
+System.out.println("addition of a&b"+addition);
+System.out.println("substraction of a&b"+substraction);
+}
+}
