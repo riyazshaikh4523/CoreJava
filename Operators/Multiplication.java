4bd80324d54a8ace249c68e03e85c8af5691d871
riyazshaikh4523 <147690876+riyazshaikh4523@users.noreply.github.com>
2023-10-17 07:00:08 +0530

    Add files via upload

---
+++ Operators/Multiplication.java
@@ -0,0 +1,12 @@
+class Multiplication{
+	public static void main(String args[]){
+int a=50;
+int b=100;
+int Multiplication=a*b;
+System.out.println("Multiplication of a&b"+Multiplication);
+
+
+
+}
+
+}
