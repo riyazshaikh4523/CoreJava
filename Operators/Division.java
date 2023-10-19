37b9249d2c6ce1c5a25964adf5663e5ae65d7e32
riyazshaikh4523 <147690876+riyazshaikh4523@users.noreply.github.com>
2023-10-19 23:21:07 +0530

    Update Division.java

--- Operators/Division.java
+++ Operators/Division.java
@@ -2,7 +2,7 @@ class Division{
 static int a=45;
 static int b=15;
 static int Division=a/b;
-public static void main(String args[]){
+public static void main(String args[]){ 
 System.out.println("Division of a&b"+Division);
 
 
