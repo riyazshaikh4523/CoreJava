78232e60f82594d94484ab068e6eb89f3e2a42c4
riyazshaikh4523 <147690876+riyazshaikh4523@users.noreply.github.com>
2023-10-19 23:20:31 +0530

    Update Multiplication.java

--- Operators/Multiplication.java
+++ Operators/Multiplication.java
@@ -1,7 +1,7 @@
 class Multiplication{
 	public static void main(String args[]){
 int a=50;
-int b=100;
+int b=100; 
 int Multiplication=a*b;
 System.out.println("Multiplication of a&b"+Multiplication);
 
