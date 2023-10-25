5c486ec6cfb73369d13a3f83822bdc4bdc21b4a9
riyazshaikh4523 <147690876+riyazshaikh4523@users.noreply.github.com>
2023-10-25 12:11:47 +0530

    Add files via upload

---
+++ Controlflow/SwitchStatement/SwitchStatementEx02.java
@@ -0,0 +1,36 @@
+package switchstatement;
+//Day of the Week: Create a program that takes an integer input (1-7)
+//representing a day of the week and outputs the corresponding day using a
+//switch-case statement.
+public class SwitchStatementEx02 {
+
+	public static void main(String[] args) {
+		int Days =1;
+		switch(Days) {
+		
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+			
+		}
+	}
+
+}
