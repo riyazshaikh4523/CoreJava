492b77740cc5c8eaf921feee3a9fe2e3b658e185
riyazshaikh4523 <147690876+riyazshaikh4523@users.noreply.github.com>
2023-10-25 12:11:01 +0530

    1 program added in switchStatement

---
+++ Controlflow/SwitchStatement/SwitchStatementEx01.java
@@ -0,0 +1,56 @@
+package switchstatement;
+//Write a program that takes an integer input (1-12)representing a month and
+		//outputs the corresponding month's name using a
+		//switch-case statement.
+
+public class SwitchStatementEx01 {
+
+	public static void main(String[] args) {
+			
+			int month=9;
+			switch(month){
+
+			case 1:
+				System.out.println("January");
+				break;
+			case 2:
+				System.out.println("feb");
+				break;
+			case 3:
+				System.out.println("march");
+				break;
+			case 4:
+				System.out.println("apr");
+				break;
+			case 5:
+				System.out.println("may");
+				break;
+			case 6:
+				System.out.println("june");
+				break;
+			case 7:
+				System.out.println("july");
+				break;
+			case 8:
+				System.out.println("aug");
+				break;
+			case 9:
+				System.out.println("sep");
+				break;
+			case 10:
+				System.out.println("oct");
+				break;
+			case 11:
+				System.out.println("nov");
+				break;
+			case 12:
+				System.out.println("dec");
+				break;
+
+			}
+
+			}
+			
+	}
+
+
