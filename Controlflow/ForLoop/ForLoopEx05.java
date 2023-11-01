ce718e20d7422cfe0251551096d069ed3d9fdbd0
riyazshaikh4523 <147690876+riyazshaikh4523@users.noreply.github.com>
2023-11-01 22:58:40 +0530

    Add files via upload

---
+++ Controlflow/ForLoop/ForLoopEx05.java
@@ -0,0 +1,18 @@
+package forloopassignment;
+//11. Positive Integers: Create a program that prints all the positive  
+//integers less than 20 using a `for` loop.
+public class ForLoopEx05 {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=20;
+		for(a=1;a<b;a++) {
+			if (a>0 && a<20) {
+				System.out.println(a);
+			}
+			
+		
+		}
+	}
+
+}
---
+++ Controlflow/ForLoop/ForLoopEx06.java
@@ -0,0 +1,17 @@
+package forloopassignment;
+//15. Print Alphabets: Create a program to print all the lowercase English  alphabets using a `for` loop.
+public class ForLoopEx06 {
+
+	public static void main(String[] args) {
+		char ch1='a';
+		char ch2='z';
+		for(ch1='a'; 'a'<'z'; ch1++) {
+			System.out.println(ch1 + " ");
+		}
+		
+		}
+}
+	
+
+
+
